ee9835aca572c47a571c19f1f1a2355e6dbc0403
backstone025 <112708588+backstone025@users.noreply.github.com>
2026-07-06 11:51:27 +0900

    fix: auth 도메인 에러에 대해 호출 시, 에러 코드와 메세지, HTTP 상태 반환하도록 수정

--- src/main/java/com/fitu/benefitu/domain/users/errors/AuthException.java
+++ src/main/java/com/fitu/benefitu/domain/users/errors/AuthException.java
@@ -20,16 +20,16 @@ public enum AuthException implements BaseErrorCode {
 
     @Override
     public String getCode() {
-        return "";
+        return code;
     }
 
     @Override
     public String getMessage() {
-        return "";
+        return message;
     }
 
     @Override
     public HttpStatus getStatus() {
-        return null;
+        return httpStatus;
     }
 }
