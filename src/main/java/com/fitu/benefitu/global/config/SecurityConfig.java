abff3a0bde8562bb788a49fecf3a43149786de9e
backstone025 <112708588+backstone025@users.noreply.github.com>
2026-07-10 19:11:45 +0900

    fix: OPTIONS 요청에 대한 CORS 허용 수정

--- src/main/java/com/fitu/benefitu/global/config/SecurityConfig.java
+++ src/main/java/com/fitu/benefitu/global/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.fitu.benefitu.global.response.ApiResponse;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -30,6 +31,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(HttpMethod.OPTIONS).permitAll()
                         .requestMatchers(
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
